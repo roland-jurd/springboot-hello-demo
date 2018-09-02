3fd43c808064da632102b2e873b2f9ad68a81ce6
roland-jurd <rbjurd@gmail.com>
2018-09-02 20:04:56 +0200

    initial commit

---
+++ src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.demo.controller;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    
+    
+    @GetMapping("/test")
+    public String test() throws UnknownHostException {
+        return "Hello from " + InetAddress.getLocalHost().getHostName();
+    }
+    
+}
